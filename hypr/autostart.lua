7f017bf92520130b7e2d1e395668f7ea2527841a
TheBubb <caseyultsch1234@gmail.com>
2026-07-24 15:38:56 -0700

    Add files via upload

---
+++ hypr/autostart.lua
@@ -0,0 +1,45 @@
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+-- Set programs that you use
+local terminal    = "kitty"
+local fileManager = "dolphin"
+local menu        = "hyprlauncher"
+
+
+-------------------
+---- AUTOSTART ----
+-------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+--
+ hl.on("hyprland.start", function () 
+   hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP") -- must be first
+
+   hl.exec_cmd(terminal)
+   hl.exec_cmd("nm-applet")
+   hl.exec_cmd("waybar & hyprpaper")
+   hl.exec_cmd("systemctl --user start hyprpolkitagent")
+   hl.exec_cmd("dunst")
+   hl.exec_cmd("wl-paste --type text --watch cliphist store")
+   hl.exec_cmd("wl-paste --type image --watch cliphist store") 
+   hl.exec_cmd("walker --gapplication-service")
+   hl.exec_cmd("systemctl --user start elephant.service")
+end)
+
+
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+-- -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
