0723726d7194359464b6ea7466aaa4f5b7237953
TheBubb <caseyultsch1234@gmail.com>
2026-07-23 23:45:44 -0700

    updated gaps in look.lua

--- hypr/config/look.lua
+++ hypr/config/look.lua
@@ -5,8 +5,8 @@
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
     general = {
-        gaps_in  = 5,
-        gaps_out = 20,
+        gaps_in  = 10,
+        gaps_out = 25,
 
         border_size = 2,
 
